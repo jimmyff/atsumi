d100760e0ef7bb14374093f5299f0d3066c9281e
Jimmy Forrester-Fellowes <jimmy@rocketware.co.uk>
2016-02-11 16:11:28 +0000

    added a few minor options for disabled button state & input text events

--- classes/widgets/form/elements/widget_TextElement.php
+++ classes/widgets/form/elements/widget_TextElement.php
@@ -6,7 +6,9 @@ class widget_TextElement extends widget_AbstractElement {
 	protected $cssClassName 	= 'inputText';
 	protected $placeholder		= '';
 	protected $onChange			= null;
-	protected $onKeydown		= null;
+    protected $onKeyup		    = null;
+    protected $onKeydown		= null;
+    protected $onKeypress		= null;
 	protected $onFocus			= null;
 	protected $onBlur			= null;
 	protected $disabled			= false;
@@ -22,9 +24,16 @@ class widget_TextElement extends widget_AbstractElement {
 		
 		if (array_key_exists('onChange', $args) && strlen($args['onChange']))
 			$this->onChange = $args['onChange'];
-			
-		if (array_key_exists('onKeydown', $args) && strlen($args['onKeydown']))
-			$this->onKeydown = $args['onKeydown'];
+
+        if (array_key_exists('onKeydown', $args) && strlen($args['onKeydown']))
+            $this->onKeydown = $args['onKeydown'];
+
+
+        if (array_key_exists('onKeyup', $args) && strlen($args['onKeyup']))
+            $this->onKeyup = $args['onKeyup'];
+
+        if (array_key_exists('onKeypress', $args) && strlen($args['onKeypress']))
+            $this->onKeypress = $args['onKeypress'];
 			
 		if (array_key_exists('onFocus', $args) && strlen($args['onFocus']))
 			$this->onFocus = $args['onFocus'];
@@ -51,7 +60,7 @@ class widget_TextElement extends widget_AbstractElement {
 				$attributesHtml.= sf(' %s="%s" ', $key, $val);
 		
 		
-		return(sf('<input type="%s" name="%s" value="%s" %s id="form_%s" class="%s" %s%s%s%s%s%s%s />',
+		return(sf('<input type="%s" name="%s" value="%s" %s id="form_%s" class="%s" %s%s%s%s%s%s%s%s%s />',
 			$this->htmlType,
 			$this->getName(),
 			parent::makeInputSafe($this->getValue()),
@@ -61,8 +70,10 @@ class widget_TextElement extends widget_AbstractElement {
 			$attributesHtml,
 			strlen($this->placeholder)?sf(' placeholder="%s"', $this->placeholder):'',
 			!is_null($this->onChange) && strlen($this->onChange)?sf(' onChange="%s"', $this->onChange):'',
-			!is_null($this->onKeydown) && strlen($this->onKeydown)?sf(' onKeydown="%s"', $this->onKeydown):'',
-			!is_null($this->onFocus) && strlen($this->onFocus)?sf(' onFocus="%s"', $this->onFocus):'',
+            !is_null($this->onKeyup) && strlen($this->onKeyup)?sf(' onKeyup="%s"', $this->onKeyup):'',
+            !is_null($this->onKeydown) && strlen($this->onKeydown)?sf(' onKeydown="%s"', $this->onKeydown):'',
+            !is_null($this->onKeypress) && strlen($this->onKeypress)?sf(' onKeypress="%s"', $this->onKeypress):'',
+            !is_null($this->onFocus) && strlen($this->onFocus)?sf(' onFocus="%s"', $this->onFocus):'',
 			!is_null($this->onBlur) && strlen($this->onBlur)?sf(' onBlur="%s"', $this->onBlur):'',
 			$this->disabled?' disabled':''
 		));
